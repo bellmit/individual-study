b941bfb4fdc5c784a7ef7763857f967a817e6fd8
gyw <gyw@163.com>
2021-09-26 19:38:08 +0800

    feat:new db utils

---
+++ platform/gorm/src/main/java/cn/gyw/platform/gorm/config/RootConfig.java
@@ -0,0 +1,11 @@
+package cn.gyw.platform.gorm.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"cn.gyw.handwritten.gorm"})
+public class RootConfig {
+
+
+}
--- platform/rpa/README.md
+++
@@ -1,6 +0,0 @@
-# RPA
-
-参考：
-https://github.com/robotframework/robotframework
-
-java -jar robotframework-3.0.jar mytests.robot
