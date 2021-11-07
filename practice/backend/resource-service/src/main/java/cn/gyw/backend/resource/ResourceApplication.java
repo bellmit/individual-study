c12c97f45462159e626cc499917540f345e7d8ca
masgyw <masgyw@163.com>
2021-11-07 15:26:57 +0800

    feat: 配置文件本地化读取

--- practice/backend/resource-service/src/main/java/cn/gyw/backend/resource/ResourceApplication.java
+++ practice/backend/resource-service/src/main/java/cn/gyw/backend/resource/ResourceApplication.java
@@ -1,8 +1,13 @@
 package cn.gyw.backend.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 /**
  * @description 资源服务
@@ -20,5 +25,16 @@ public class ResourceApplication {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner(@Autowired Environment environment,
+                                               @Value("${crawler.ds.host}") String dsUrl,
+                                               @Value("${crawler.ds.password}") String pwd) {
+        return args -> {
+            System.out.println("env.local.name :" + environment.getProperty("env.local.name"));
+            System.out.println("dsUrl:" + dsUrl);
+            System.out.println("pwd:" + pwd);
+        };
     }
 }
